a6a5bacedf17d87478dea5f70d9d4ed7fcd9f314
setiawanshandy <shandy.setiawan@qasir.id>
2021-07-06 21:39:39 +0700

    first commit

---
+++ module1/s1/app.rb
@@ -0,0 +1,21 @@
+require_relative 'person'
+
+jin = Person.new("Jin Sakai", 100, 50)
+puts jin
+
+khotun = Person.new("Khotun Khan", 500, 50)
+puts khotun
+
+loop do
+
+jin.attack(khotun)
+puts khotun
+break if khotun.dies?
+
+khotun.attack(jin)
+puts jin
+break if jin.dies?
+
+end
+# khotun.hitpoint -= jin.attack_damage
+# puts khotun
