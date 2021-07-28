bd6af8bc30f271996d8a6196cc1809a6bf19831e
setiawanshandy <shandysetiawanliu@gmail.com>
2021-07-28 19:14:27 +0700

    test case array of 3

--- module4/s2/spec/number_spec.rb
+++ module4/s2/spec/number_spec.rb
@@ -2,13 +2,23 @@ require_relative '../src/number'
 
 RSpec.describe Number do
 
-    it 'increment number' do
+    it 'increment number array of 1' do
         #given
-    number = Number.new([1,2,3])
+    number = Number.new([0])
         #when
     increment = number.increment
         #then
-    expect(increment).to eq([1,2,3,4])
+    expect(increment).to eq([1])
+    end
+
+
+    it 'increment number array of 2' do
+        #given
+    number = Number.new([1,2])
+        #when
+    increment = number.increment
+        #then
+    expect(increment).to eq([1,3])
     end
 
 end
