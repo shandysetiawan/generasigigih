09833387c3b43e5d555cabaf8cd2a88d585c9a7f
setiawanshandy <shandysetiawanliu@gmail.com>
2021-07-28 19:15:05 +0700

    test case array of 4

--- module4/s2/spec/number_spec.rb
+++ module4/s2/spec/number_spec.rb
@@ -21,4 +21,23 @@ RSpec.describe Number do
     expect(increment).to eq([1,3])
     end
 
+    it 'increment number array of 3' do
+        #given
+    number = Number.new([1,2,3])
+        #when
+    increment = number.increment
+        #then
+    expect(increment).to eq([1,2,4])
+    end
+
+
+    it 'increment number array of 4' do
+        #given
+    number = Number.new([1,2,5,4])
+        #when
+    increment = number.increment
+        #then
+    expect(increment).to eq([1,2,5,5])
+    end
+
 end
